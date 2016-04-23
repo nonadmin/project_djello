49388b16be51f919d5b7055d4376cf1f2e4ea27e
Justin Mullis <justin.mullis@gmail.com>
2016-04-23 11:50:16 -0400

    devise initial configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+3.times do |i|
+  User.create!(
+    username: "Dixie#{i}",
+    email: "test#{i}@example.com",
+    password: "password",
+    password_confirmation: "password")
+end
